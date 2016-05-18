6b2a547b6c1ae74cc5431699809ad2f2701950c4
giang1807 <hoanggianghtg.nuce@gmail.com>
2016-05-19 00:19:59 +0700

    CapNhatNgonNgu

---
+++ Models/Dao/NgonNguDao.cs
@@ -0,0 +1,77 @@
+﻿using Models.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Dao
+{
+   public class NgonNguDao
+    {
+
+        private ShoeShopDbContext db = null;
+        public NgonNguDao()
+        {
+            db = new ShoeShopDbContext();
+        }
+        public int Insert(Language entity)
+        {
+            db.Languages.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                var res = db.Languages.Find(id);
+                db.Languages.Remove(res);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+        }
+        public IPagedList<Language> SearchResult(string searchKey, int pageNum = 1, int pageSize = 10)
+        {
+            var res = db.Languages.Where(o => o.LanguageName.Contains(searchKey));
+            return res.OrderByDescending(o => o.LanguageName).ToPagedList<Language>(pageNum, pageSize);
+        }
+        public Language ViewDetail(int id)
+        {
+            return db.Languages.Find(id);
+        }
+        public bool Update(Language ab)
+        {
+            try
+            {
+                var res = db.Languages.Find(ab.ID);
+                res.LanguageName = ab.LanguageName;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+
+        }
+        public IPagedList<Language> ListAll(int page, int pageSize)
+        {
+            return db.Languages.OrderByDescending(o => o.LanguageName).ToPagedList(page, pageSize);
+        }
+        public IEnumerable<Language> ListAll()
+        {
+            return db.Languages.OrderByDescending(o => o.LanguageName);
+        }
+    }
+
+}
+
---
+++ ShoeShop/Areas/admin/Controllers/NgonNguController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models.EF;
+using Models.Dao;
+using ShoeShop.Areas.admin.Code;
+using ShoeShop.Common;
+
+namespace ShoeShop.Areas.admin.Controllers
+{
+    public class NgonNguController : BaseController
+    {
+        public ActionResult Index(string searchKey, int page = 1, int pagesize = 10)
+        {
+            var ab = new NgonNguDao();
+            if (!string.IsNullOrEmpty(searchKey))
+            {
+                var lst = ab.SearchResult(searchKey, page, pagesize);
+                return View(lst);
+            }
+            else
+            {
+                var model = ab.ListAll(page, pagesize);
+                return View(model);
+            }
+        }
+        public ActionResult Details(int id)
+        {
+
+            return View();
+        }
+
+        // GET: admin/NgonNgu/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: admin/NgonNgu/Create
+        [HttpPost]
+        public ActionResult Create(Language collection)
+        {
+            var session = (UserSession)Session[CommonConstant.USER_SESSION];
+            if (ModelState.IsValid)
+            {
+                var dao = new NgonNguDao();
+                long id = dao.Insert(collection);
+                if (id > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", CommonConstant.INSERT_FAIL);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", CommonConstant.INSERT_FAIL);
+            }
+            return View("Index");
+        }
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase Image)
+        {
+            string path = Server.MapPath(Image.FileName);
+            Image.SaveAs(path);
+            return View("Create");
+        }
+        // GET: admin/About/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var ab = new NgonNguDao().ViewDetail(id);
+            return View(ab);
+        }
+
+        // POST: admin/NgonNgu/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Language ab)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new NgonNguDao();
+                var res = dao.Update(ab);
+                if (res)
+                {
+                    return RedirectToAction("Index", "NgonNgu");
+                }
+                else
+                {
+                    ModelState.AddModelError("", CommonConstant.UPDATE_FAIL);
+                }
+            }
+            return View("Index");
+        }
+
+        // GET: admin/NgonNgu/Delete/5
+        public ActionResult Delete(int id)
+        {
+            new NgonNguDao().Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        // POST: admin/NgonNgu/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string[] Ids)
+        {
+            try
+            {
+                foreach (var id in Ids)
+                {
+                    new NgonNguDao().Delete(int.Parse(id));
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ModelState.AddModelError("", "có lỗi");
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
